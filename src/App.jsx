87d40f6c3bc254646bdb7455f5562c03937c2401
njapthavlu <sandroni.lucas.javier@gmail.com>
2025-11-01 05:44:33 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DonAnteroHome from './components/DonAnteroHome';
 import DonAnteroCatalogGrid from './components/DonAnteroCatalogGrid';
 import DonAnteroProductPage from './components/DonAnteroProductPage';
+import CotizacionPage from './components/CotizacionPage';
 
 export default function App() {
   return (
@@ -11,6 +12,8 @@ export default function App() {
         <Route path="/" element={<DonAnteroHome />} />
         <Route path="/catalogo" element={<DonAnteroCatalogGrid />} />
         <Route path="/producto/:slug" element={<DonAnteroProductPage />} />
+        <Route path="/cotizacion" element={<CotizacionPage />} />
+
       </Routes>
     </Router>
   );
