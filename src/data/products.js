99332863e69b5cc6d669b109ca17361999b69201
njapthavlu <sandroni.lucas.javier@gmail.com>
2025-11-28 11:18:06 -0300

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -15,6 +15,9 @@ import chalecoGabardinaReflectivoImg from '../images/chaleco-de-tela-gabardina-c
 import ambosIndustriaAlimenticiaImg from '../images/ambos-industria-alimenticia-velcro-elastico-gabardina.jpg';
 import mametertrucker from '../images/mame-ter-trucker.jpg';
 import camperforrada from '../images/camper-forrada.jpg';
+import pantgabardina from '../images/pantalon-gabardina.jpg';
+import pantclasico from '../images/pantalon-clasico.jpg';
+import camperatruckerr from '../images/campera-truckerr.jpg';
 
 export const PRODUCTS = [
   {
@@ -378,7 +381,7 @@ camperforrada
       "matelase"
     ],
     "images": [
-      "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='1200' height='1600'><defs><linearGradient id='g' x1='0' y1='0' x2='1' y2='1'><stop stop-color='%233a86ff' offset='0'/><stop stop-color='%23143f6b' offset='1'/></linearGradient></defs><rect width='100%' height='100%' fill='url(%23g)'/></svg>"
+camperatruckerr
     ],
     "summary": "Campera trucker con forrería matelassé 150g.",
     "specs": [
@@ -462,7 +465,7 @@ camperforrada
       "pantalon"
     ],
     "images": [
-      "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='1200' height='1600'><defs><linearGradient id='g' x1='0' y1='0' x2='1' y2='1'><stop stop-color='%230e2a47' offset='0'/><stop stop-color='%23257cff' offset='1'/></linearGradient></defs><rect width='100%' height='100%' fill='url(%23g)'/></svg>"
+pantgabardina
     ],
     "summary": "Pantalón cargo de gabardina sanforizada con múltiples bolsillos.",
     "specs": [
@@ -602,7 +605,7 @@ camperforrada
       "pantalon"
     ],
     "images": [
-      "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='1200' height='1600'><defs><linearGradient id='g' x1='0' y1='0' x2='1' y2='1'><stop stop-color='%230e2a47' offset='0'/><stop stop-color='%2381a9ff' offset='1'/></linearGradient></defs><rect width='100%' height='100%' fill='url(%23g)'/></svg>"
+pantclasico
     ],
     "summary": "Pantalón clásico de gabardina sanforizada 8 oz.",
     "specs": [
